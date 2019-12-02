cb1c715bd06a0d2b4d8e8c2fb9dde76ae4161228
Max <antiarcher.gmail.com>
2019-12-02 23:41:51 +0200

    update client

--- worker/index.js
+++ worker/index.js
@@ -1,6 +1,6 @@
 var CronJob = require('cron').CronJob;
 
 var fetchGithub = require('./tasks/fetch-github')
-
+fetchGithub();
 //звездочки это то как часто вы хотите чтобы ваша работа выполнялась
-new CronJob('*/1 * * * *', fetchGithub, null, true, 'America/Los_Angeles');
+new CronJob('*/20 * * * *', fetchGithub, null, true, 'America/Los_Angeles');
--- worker/tasks/fetch-github.js
+++ worker/tasks/fetch-github.js
@@ -59,6 +59,4 @@ async function fetchGithub() {
   console.log({success});
 }
 
-fetchGithub();
-
 module.exports = fetchGithub;
